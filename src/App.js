314a0c9d3544a7a59460a221f10260cffe350548
Franca <francaafigueroa@gmail.com>
2022-08-26 23:28:06 -0300

    navbar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+import './app/styles.css'
+const App = () => {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return (
+    <div class="navbar">
+        <div class="title">Franca Figueroa</div>
+        <div class="navbar-links">
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Productos</a>
+            </li>
+            <li>
+                <a href="#">Nosotros</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    );
+}
+export default NavBar;
