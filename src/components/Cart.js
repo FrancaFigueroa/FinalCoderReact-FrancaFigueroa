fc3f3ce35e51604956773e4a807206cee32ee32a
Franca <francaafigueroa@gmail.com>
2022-10-04 23:16:23 -0300

    item-collection-2

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../CartContext';
@@ -5,6 +6,25 @@ import ItemCart from './ItemCart';
 
 const Cart = () => {
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            name: ' Pablo',
+            email: 'pablo@gmail.com',
+            phone: 12345,
+            adress: 'Pablito'
+        },
+        items: cart.map(planta => ({ id: planta.id, title: planta.title, price: planta.price, quantity: planta.quantity })),
+        total: totalPrice(),
+
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+            .then(({ id }) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return (
@@ -20,6 +40,7 @@ const Cart = () => {
                 Cart.map(product => <ItemCart key={product.id} product={product} />)
             }
             <p>total: ${totalPrice()}</p>
+            <button onClick={handleClick}>generar orden</button>
         </>
     )
 }
